0057e086ee267981dabb3bbcb844dee9c93635d7
lap <congdinh.lap@gmail.com>
2023-06-17 16:18:13 +0700

    update banner slide

--- src/components/music/ MusicPlayer.jsx
+++ src/components/music/ MusicPlayer.jsx
@@ -10,10 +10,6 @@ const MusicPlayer = () => {
     return (
         <div>
             <iframe src="https://www.nhaccuatui.com/mh/background/VyfpdksboLsO" width="1" height="1" frameborder="0" allowfullscreen allow="autoplay"></iframe>
-            <audio ref={audioRef} src="I-DO.mp3" loop>
-
-                Your browser does not support the audio element.
-            </audio>
         </div>
     );
 };
