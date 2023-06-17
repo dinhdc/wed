3ab7a42f78ed59955f353f452a36b049c57727df
lap <congdinh.lap@gmail.com>
2023-06-17 16:19:58 +0700

    update banner slide

--- src/components/music/ MusicPlayer.jsx
+++ src/components/music/ MusicPlayer.jsx
@@ -1,11 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 
 const MusicPlayer = () => {
-    const audioRef = useRef(null);
-
-    useEffect(() => {
-        audioRef.current.play();
-    }, []);
 
     return (
         <div>
