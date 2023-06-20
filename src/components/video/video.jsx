dd09923d186a5d64d572b75401bd84b5d9da7ce4
cong dinh <congdinhpro2k@gmail.com>
2023-06-20 20:58:48 +0700

    update

--- src/components/video/video.jsx
+++ src/components/video/video.jsx
@@ -6,7 +6,7 @@ const Video = () => {
             <h2>Xem video cưới của chúng tôi</h2>
             <p>Tình yêu không làm cho thế giới quay tròn. Tình yêu là những gì làm cho chuyến đi đáng giá.</p>
             <div className="video-btn">
-                <a href="https://www.youtube.com/watch?v=f6ZfmGNV8d8" rel="noreferrer" target="_blank"
+                <a href="https://www.youtube.com/watch?v=D6VNJLQIU_U" rel="noreferrer" target="_blank"
                    className="video-play-btn">
                     <i className="ti-control-play"></i></a></div>
         </div>
